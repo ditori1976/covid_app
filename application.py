0323bc5212cf90e02072293275b4df86b287d742
Goldstein <ditori@gmail.com>
2020-03-31 16:55:51 +0200

    init

--- application.py
+++ application.py
@@ -1,5 +1,38 @@
+#!/usr/bin/env python3
+
+"""
+Author: Dirk Riemann, 2020
+
+COVID-19 dashboard
+based on dash package,
+using plotly graph objects
+and mapbox API for mapping
+
+development/debuggin mode
+NOT FOR PRODUCTION
+"""
+
+# import packages
+import os
 import dash
+from dash.dependencies import Input, Output, State
+import dash_core_components as dcc
+import dash_bootstrap_components as dbc
 import dash_html_components as html
+import dash_table
+import datetime
+import pandas as pd
+import plotly.graph_objects as go
+from urllib.request import urlopen
+import json
+
+if "HOST" in os.environ:
+    host = os.environ.get("HOST")
+else:
+    host = "127.0.0.1"
+
+MAPBOX = os.environ.get("MAPBOX")
+print(MAPBOX)
 
 app = dash.Dash(__name__)
 
@@ -7,7 +40,7 @@ app = dash.Dash(__name__)
 #app.css.config.serve_locally = True
 
 app.layout = html.Div([
-    html.H1('Hello, World!')
+    html.H1(MAPBOX)
 ])
 
 application = app.server
