a865f87a9c89ef34160b0a95e8135c4ca06ac41f
ditori <ditori@gmail.com>
2020-04-01 15:42:44 +0000

    update

--- application.py
+++ application.py
@@ -19,26 +19,21 @@ from dash.dependencies import Input, Output, State
 import dash_core_components as dcc
 import dash_bootstrap_components as dbc
 import dash_html_components as html
-import dash_table
-import datetime
+
+# import dash_table
 import pandas as pd
 import plotly.graph_objects as go
-from urllib.request import urlopen
-import json
-import requests
-import sys
-import io
+
+# import sys
 
 
-dirname = os.path.abspath(__file__)  # dirname
-dirname = os.path.dirname(os.path.abspath(__file__))
-
+# dirname = os.path.dirname(os.path.abspath(__file__))
 
 if "HOST" in os.environ:
     host = os.environ.get("HOST")
 else:
     host = "127.0.0.1"
-    
+
 if "PORT" in os.environ:
     port = os.environ.get("PORT")
 else:
@@ -47,103 +42,70 @@ else:
 
 MAPBOX = os.environ.get("MAPBOX")
 
-with urlopen(
-    "https://raw.githubusercontent.com/mapbox/geojson-vt-cpp/master/data/countries.geojson"
-) as response:
-    countries = json.load(response)
+# load data
+from load_data import ecdc_raw, countries, per_country, summary_country
 
-url = "https://opendata.ecdc.europa.eu/covid19/casedistribution/csv"
-s = requests.get(url).content
-ecdc_raw = pd.read_csv(io.StringIO(s.decode('utf-8')))
-
-ecdc_raw.rename(columns={'countriesAndTerritories': 'country'}, inplace=True)
-ecdc_raw.iloc[:, 6].replace(
-    to_replace=r'_', value=' ', regex=True, inplace=True)
-ecdc_raw.loc[:, 'country'].replace(
-    to_replace=r'Russia', value='Russian Federation', regex=True, inplace=True)
-
-countries_codes = pd.read_csv(dirname + '/data/country_codes.csv')
-
-countries_codes.rename(columns={'name': 'country',
-                                'alpha-3': 'iso_alpha',
-                                'alpha-2': 'geoId',
-                                'country-code': 'iso_num'}, inplace=True)
-countries_codes.drop(columns=['iso_3166-2', 'intermediate-region', 'region-code',
-                              'sub-region-code', 'intermediate-region-code'], inplace=True)
-
-countries_codes.loc[countries_codes.geoId == 'KP', 'country'] = 'North Korea'
-countries_codes.loc[countries_codes.geoId == 'KR', 'country'] = 'South Korea'
-countries_codes.loc[countries_codes.geoId == 'VN', 'country'] = 'Vietnam'
-countries_codes.loc[countries_codes.geoId == 'IR', 'country'] = 'Iran'
-countries_codes.loc[countries_codes.geoId ==
-                    'GB', 'country'] = 'United Kingdom'
-
-countries_un = pd.read_csv(dirname + '/data/countries.csv')
-countries_un.rename(columns={'name': 'country'}, inplace=True)
-countries_un.drop(columns=['Rank', 'pop2018', 'Density'], inplace=True)
-countries_un.loc[:, 'pop2019'] = countries_un.pop2019.mul(1000)
-countries_un.loc[:, 'country'].replace(
-    to_replace=r'United States', value='United States of America', regex=True, inplace=True)
-countries_un.loc[:, 'country'].replace(
-    to_replace=r'Russia', value='Russian Federation', regex=True, inplace=True)
-
-per_country = ecdc_raw.iloc[:, [0, 4, 5]].groupby(ecdc_raw.iloc[:, 6]).sum()
-
-#
-summary_country = pd.merge(per_country, countries_un,
-                           how='left', on=['country'])
-summary_country = pd.merge(
-    summary_country, countries_codes,  how='left', on=['country'])
-summary_country.loc[:, 'Cases/Mio. capita'] = (summary_country.cases /
-                                               summary_country.pop2019*1000000).round(0)
-summary_country.loc[:, 'Deaths/Mio. capita'] = (summary_country.deaths /
-                                                summary_country.pop2019*1000000).round(0)
-
-
-layout = go.Layout(yaxis=dict(type="linear", autorange=True))
-layout_log = go.Layout(yaxis=dict(type="linear", autorange=True))
-
-all_infections_deaths = ecdc_raw.iloc[:, [
-    0, 4, 5]].groupby(["dateRep"]).sum().cumsum()
-
-fig = go.Figure(
-    data=[
-        go.Scatter(name="Cases", x=all_infections_deaths.index,
-                   y=all_infections_deaths.iloc[:, 0]),
-        go.Scatter(name="Deaths", x=all_infections_deaths.index,
-                   y=all_infections_deaths.iloc[:, 1]),
-    ],
-    layout=layout,
-)
-fig.update_layout(legend_orientation="h", legend=dict(
-    x=0, y=.95), plot_bgcolor='white', autosize=True, height=400)
+all_infections_deaths = ecdc_raw.iloc[:, [0, 4, 5]].groupby(["dateRep"]).sum().cumsum()
 
 per_country = ecdc_raw.iloc[:, [0, 4, 5]].groupby(ecdc_raw.iloc[:, 6]).sum()
 min_cases = 20000
 per_country_max = per_country[per_country.iloc[:, 0] > min_cases]
-per_country_max = per_country_max.sort_values('cases')
-fig_cc = go.Figure(data=[go.Bar(name='confirmed infections', x=per_country_max.index,
-                                y=per_country_max.iloc[:, 0]),
-                         go.Bar(name='deaths', x=per_country_max.index,
-                                y=per_country_max.iloc[:, 1])],
-                   layout=layout_log)
-fig_cc.update_layout(legend_orientation="h", legend=dict(
-    x=0, y=.95), plot_bgcolor='white', autosize=True, height=400)
+per_country_max = per_country_max.sort_values("cases")
 
-app = dash.Dash(__name__, external_stylesheets=[
-                dbc.themes.BOOTSTRAP, dbc.themes.GRID])
+# layout
+layout = go.Layout(yaxis=dict(type="linear", autorange=True))
+layout_log = go.Layout(yaxis=dict(type="linear", autorange=True))
+
+fig = go.Figure(
+    data=[
+        go.Scatter(
+            name="Cases",
+            x=all_infections_deaths.index,
+            y=all_infections_deaths.iloc[:, 0],
+        ),
+        go.Scatter(
+            name="Deaths",
+            x=all_infections_deaths.index,
+            y=all_infections_deaths.iloc[:, 1],
+        ),
+    ],
+    layout=layout,
+)
+fig.update_layout(
+    legend_orientation="h",
+    legend=dict(x=0, y=0.95),
+    plot_bgcolor="white",
+    autosize=True,
+    height=400,
+)
+
+fig_cc = go.Figure(
+    data=[
+        go.Bar(
+            name="confirmed infections",
+            x=per_country_max.index,
+            y=per_country_max.iloc[:, 0],
+        ),
+        go.Bar(name="deaths", x=per_country_max.index, y=per_country_max.iloc[:, 1]),
+    ],
+    layout=layout_log,
+)
+fig_cc.update_layout(
+    legend_orientation="h",
+    legend=dict(x=0, y=0.95),
+    plot_bgcolor="white",
+    autosize=True,
+    height=400,
+)
+
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP, dbc.themes.GRID])
 
 app.layout = html.Div(
     [
         dbc.Row(
             [
                 dbc.Col(
-                    html.Img(
-                        src=app.get_asset_url("logo.png"),
-                        height=50,
-                        # style={'margin': 5},
-                    ),
-                    width=1,
+                    html.Img(src=app.get_asset_url("logo.png"), height=50), width=1,
                 ),
                 dbc.Col(html.H1("COVID-19"), width=2,),
                 dbc.Col(
@@ -154,24 +116,27 @@ app.layout = html.Div(
                                 id="value-selected",
                                 value="cases",
                                 options=[
-                                    {"label": "Cases/Mio. capita ",
-                                        "value": "cases"},
-                                    {"label": "Deaths/Mio. capita ",
-                                        "value": "deaths"},
+                                    {"label": "Cases/Mio. capita ", "value": "cases"},
+                                    {"label": "Deaths/Mio. capita ", "value": "deaths"},
                                 ],
                                 style={
                                     "display": "block",
                                     "margin-left": "auto",
                                     "margin-right": "auto",
                                     "width": "100%",
+                                    "border-width": 0,
+                                    "background-color": "#faf9f7",
+                                    "text-align": "center",
+                                    "width": 200,
                                 },
                             )
                         ],
                     ),
                     width=2,
+                    style={"margin-top": 20},
                 ),
             ],
-            justify="start",
+            justify="center",
             no_gutters=True,
         ),
         dbc.Row(
@@ -180,46 +145,43 @@ app.layout = html.Div(
                     html.Div(
                         id="div-main-map",
                         children=[
-                            dcc.Graph(id="main-map",
-                                      style={"margin": 0, "padding": 0},)
+                            dcc.Graph(id="main-map", style={"margin": 0, "padding": 0},)
                         ],
                         style={"margin": 0, "padding": 0},
                     ),
                     width=6,
                     style={"margin": 0},
                 )
-            ]
+            ],
+            justify="center",
         ),
         dbc.Row(
             [
                 dbc.Col(
                     [
                         html.Div(
-                            id="div-main-graph", children=[dcc.Graph(id="main-graph", figure=fig), ]
+                            id="div-main-graph",
+                            children=[dcc.Graph(id="main-graph", figure=fig),],
                         )
                     ],
                     width=6,
                 ),
-            ]
+            ],
+            justify="center",
         ),
         dbc.Row(
             [
                 dbc.Col(
                     [
                         html.Div(
-                            id="div-minor-graph", children=[dcc.Graph(id="minor-graph", figure=fig_cc), ]
+                            id="div-minor-graph",
+                            children=[dcc.Graph(id="minor-graph", figure=fig_cc),],
                         )
                     ],
                     width=6,
-                    style={"margin": 0},
                 ),
-                dbc.Col(
-                    [
-
-                    ],
-                    width=6,
-                ),
-            ]
+            ],
+            justify="center",
         ),
     ]
 )
@@ -250,13 +212,11 @@ def update_figure(selected):
         zmin=0,
         zmax=limit,
         marker={"line": {"color": "rgb(180,180,180)", "width": 0.5}},
-        colorbar={"thickness": 20, "len": 0.6,
-                  "x": 0.8, "y": 0.6, "outlinewidth": 0},
+        colorbar={"thickness": 20, "len": 0.6, "x": 0.8, "y": 0.6, "outlinewidth": 0},
     )
     return {
         "data": [trace],
         "layout": go.Layout(
-            width=1000,
             height=400,
             mapbox_style="mapbox://styles/dirkriemann/ck88smdb602qa1iljg6kxyavd",
             mapbox_zoom=0.75,
@@ -268,5 +228,5 @@ def update_figure(selected):
 
 
 application = app.server
-if __name__ == '__main__':
+if __name__ == "__main__":
     application.run(debug=True, port=port, host=host)
