bb3c91f33764f7e4c82f7e7a72d9c9742758ffaf
Goldstein <ditori@gmail.com>
2020-04-08 19:12:20 +0200

    modfied requirements.txt

--- test_app.py
+++ test_app.py
@@ -1,9 +1,14 @@
 '''
 Unit test for app.py
 '''
+import unittest
 import app
 
 
 class TestApp:
     def test_code(self):
         assert 20000 == app.min_cases
+
+
+if __name__ == '__main__':
+    unittest.main()
