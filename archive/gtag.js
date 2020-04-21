b6857e20a364c893a982feb488821ef7f97305bc
Goldstein <ditori@gmail.com>
2020-04-21 22:05:42 +0200

    ga

--- archive/gtag.js
+++ archive/gtag.js
@@ -1,6 +1,7 @@
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-EY291WCM2T"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
+< !--Global site tag(gtag.js) - Google Analytics-- >
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-EY291WCM2T"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
