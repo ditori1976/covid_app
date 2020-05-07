b8f44ff6549509bb5e60ec3bb28db3b6aa836810
Goldstein <ditori@gmail.com>
2020-05-07 16:34:39 +0200

    introducing clean code

---
+++ application/data/data_loader.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+import pandas as pd
+from configparser import ConfigParser
+from urllib.request import urlopen
+import requests
+import json
+from bs4 import BeautifulSoup
