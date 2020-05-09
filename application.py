bdc8cfe8a0c66f1a4f644ec41dda5c5c4187e8ff
Goldstein <ditori@gmail.com>
2020-05-09 06:39:29 +0200

    wsgi file

--- application.py
+++
@@ -1,9 +0,0 @@
-from application import app, start_multi, configuration
-
-if __name__ == "__main__":
-
-    start_multi()
-    app.run_server(
-        debug=True,
-        port=configuration.port,
-        host=configuration.host)
---
+++ application/assets/gtag.js
@@ -0,0 +1,9 @@
+< !--Global site tag(gtag.js) - Google Analytics-- >
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-EY291WCM2T"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-EY291WCM2T');
+</script>
