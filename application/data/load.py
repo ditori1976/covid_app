0688d16ab8f5b0c71f95bdb3143d6d09dfd24332
Goldstein <ditori@gmail.com>
2020-07-06 11:01:15 +0200

    add trend

--- application/data/load.py
+++ application/data/load.py
@@ -2,6 +2,10 @@ from configparser import ConfigParser
 import datetime
 from application.data.transform import Transform
 import pandas as pd
+import logging
+from statsmodels.tsa.seasonal import seasonal_decompose
+
+log = logging.getLogger("my-logger")
 
 
 class Load(Transform):
@@ -29,7 +33,8 @@ class Load(Transform):
         #
         # adds columns with values for indicators as calculated from "attributes"
         #
-        print("indicator")
+        # print("indicator")
+
         if len(attributes) == 2:
 
             if function == "diff":
@@ -61,10 +66,14 @@ class Load(Transform):
 
                 if (function == "trend") and (len(attributes) == 1):
                     data.sort_values(["region", "date"], inplace=True)
-                    data.loc[:, name] = data.loc[:, attributes[0]].diff()
-                    data.loc[:, name] = data.loc[:,
-                                                 name].pct_change(periods=14)
-                    data.loc[:, name] = data.loc[:, name].round(digits)
+                    decompose = seasonal_decompose(
+                        data.loc[:, attributes[0]], period=7, extrapolate_trend="freq")
+                    data.loc[:, name] = decompose.trend.diff(
+                        periods=7) / decompose.trend.mean()
+                    #data.loc[:, name] = data.loc[:, name].round(digits)
+
+        # remove negative values
+        # data.loc[data.loc[:, name] < 0, name] = 0
 
         return data
 
