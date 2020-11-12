4444a387eff07ea1ad65937401c22e4914ed0f4f
Goldstein <ditori@gmail.com>
2020-11-12 07:41:27 +0100

    remove cache

--- application/__main__.py
+++ application/__main__.py
@@ -284,20 +284,20 @@ start server
 """
 
 
-# There we describe cache congif
-cache = Cache(
-    app.server,
-    config={
-        "CACHE_TYPE": "redis",
-        # Note that filesystem cache doesn't work on systems with ephemeral
-        # filesystems like Heroku.
-        "CACHE_TYPE": "filesystem",
-        "CACHE_DIR": "cache-directory",
-        # should be equal to maximum number of users on the app at a single time
-        # higher numbers will store more data in the filesystem / redis cache
-        "CACHE_THRESHOLD": 200,
-    },
-)
+# # There we describe cache congif
+# cache = Cache(
+#     app.server,
+#     config={
+#         "CACHE_TYPE": "redis",
+#         # Note that filesystem cache doesn't work on systems with ephemeral
+#         # filesystems like Heroku.
+#         "CACHE_TYPE": "filesystem",
+#         "CACHE_DIR": "cache-directory",
+#         # should be equal to maximum number of users on the app at a single time
+#         # higher numbers will store more data in the filesystem / redis cache
+#         "CACHE_THRESHOLD": 200,
+#     },
+# )
 application = app.server
 if __name__ == "__main__":
 
